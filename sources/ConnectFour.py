09f33c2a4e599c9ac6ae17cdc3a47be5fba9f418
Michael Schweimer <schweimermichael@gmail.com>
2021-09-19 21:43:23 +0200

    Added todo and variable for win condition

--- sources/ConnectFour.py
+++ sources/ConnectFour.py
@@ -3,12 +3,13 @@ import numpy as np
 # init game field
 x = 7
 y = 6
+win_condition = np.array([1, 1, 1, 1])
 
 field = np.zeros([y, x])
 
 # init test szenario
 for i in range(2, 5):
-    field[3][i] = 1
+    field[i][5] = 1
 
 print(field)
 
@@ -21,13 +22,15 @@ def scan_field():
     for xpos in range(0, (x-3)):
         for ypos in range(0, y):
             print(field[ypos, xpos: (xpos+4)])
-            if np.array_equal(field[ypos, xpos: (xpos+4)], [1, 1, 1, 1]):
+            if np.array_equal(field[ypos, xpos: (xpos+4)], win_condition):
                 return True
+
+    # TODO: add reshape and then searching for the vertical winner
 
     return False
 
 
-set_game_marker(5, 3, 1)
+set_game_marker(5, 1, 1)
 print(field)
 result = scan_field()
 print(result)
