11efd5cf2d7bd595924ee483e8fe6967142f45fe
Michael Schweimer <schweimermichael@gmail.com>
2021-09-25 11:04:06 +0200

    Added vertical win recognition

--- sources/ConnectFour.py
+++ sources/ConnectFour.py
@@ -11,26 +11,35 @@ field = np.zeros([y, x])
 for i in range(2, 5):
     field[i][5] = 1
 
-print(field)
-
 
 def set_game_marker(xpos, ypos, player):
     field[ypos][xpos] = player
 
 
 def scan_field():
-    for xpos in range(0, (x-3)):
-        for ypos in range(0, y):
-            print(field[ypos, xpos: (xpos+4)])
-            if np.array_equal(field[ypos, xpos: (xpos+4)], win_condition):
-                return True
+    # checking game field x = 7, y = 6
+    winner_found = check_wincondition(field, x, y)
 
-    # TODO: add reshape and then searching for the vertical winner
+    # check gamefield with transponed matrix
+    # therefore rows will be columns
+    # => logic for check could be reused
+    if not winner_found:
+        winner_found = check_wincondition(field.T, y, x)
+
+    return winner_found
+
+
+def check_wincondition(game_field, x_max, y_max):
+    for xpos in range(0, (x_max-3)):
+        for ypos in range(0, y_max):
+            if np.array_equal(game_field[ypos, xpos: (xpos+4)], win_condition):
+                return True
 
     return False
 
 
 set_game_marker(5, 1, 1)
 print(field)
+print(field.T)
 result = scan_field()
 print(result)
