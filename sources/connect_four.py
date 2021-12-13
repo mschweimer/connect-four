66ac7a8c9b3d119b2ffe31da9ed511108e69d50d
Michael Schweimer <schweimermichael@gmail.com>
2021-12-13 22:00:33 +0100

    Introduced a class for the game

--- sources/connect_four.py
+++ sources/connect_four.py
@@ -1,57 +1,54 @@
 import numpy as np
 
-# init game field
-X = 7
-Y = 6
-win_condition = np.array([1, 1, 1, 1])
 
-field = np.zeros([Y, X])
+class ConnectFourGame:
+    def __init__(self):
+        self.x = 7
+        self.y = 6
+        self.win_condition = np.array([1, 1, 1, 1])
+        self.field = np.zeros([self.y, self.x])
 
+    def set_game_marker(self, xpos, ypos, player):
+        self.field[ypos][xpos] = player
 
-def set_game_marker(xpos, ypos, player):
-    field[ypos][xpos] = player
+    def scan_field(self):
+        # checking game field x = 7, y = 6
+        # print(field)
+        winner_found = self.check_wincondition(self.field, self.x, self.y)
 
+        # check gamefield with transponed matrix
+        # therefore rows will be columns
+        # => logic for check could be reused
+        if not winner_found:
+            winner_found = self.check_wincondition(self.field.T, self.y, self.x)
 
-def scan_field():
-    # checking game field x = 7, y = 6
-    # print(field)
-    winner_found = check_wincondition(field, X, Y)
+        if not winner_found:
+            transformed_field = self.transform_game_field(self.field)
+            winner_found = self.check_wincondition(transformed_field, self.x, self.y)
 
-    # check gamefield with transponed matrix
-    # therefore rows will be columns
-    # => logic for check could be reused
-    if not winner_found:
-        winner_found = check_wincondition(field.T, Y, X)
+        if not winner_found:
+            transformed_field = self.transform_game_field(self.field)
+            winner_found = self.check_wincondition(transformed_field.T, self.y, self.x)
 
-    if not winner_found:
-        transformed_field = transform_game_field(field)
-        winner_found = check_wincondition(transformed_field, X, Y)
+        return winner_found
 
-    if not winner_found:
-        transformed_field = transform_game_field(field)
-        winner_found = check_wincondition(transformed_field.T, Y, X)
+    def check_wincondition(self, game_field, x_max, y_max):
+        for xpos in range(0, (x_max-3)):
+            for ypos in range(0, y_max):
+                if np.array_equal(game_field[ypos, xpos: (xpos+4)], self.win_condition):
+                    return True
 
-    return winner_found
+        return False
 
+    def transform_game_field(self, game_field):
+        fields_to_move = self.y-1
+        columns_to_add = np.zeros([self.y, fields_to_move])
+        transformed_field = np.hstack((game_field, np.atleast_2d(columns_to_add)))
+        print(transformed_field)
 
-def check_wincondition(game_field, x_max, y_max):
-    for xpos in range(0, (x_max-3)):
-        for ypos in range(0, y_max):
-            if np.array_equal(game_field[ypos, xpos: (xpos+4)], win_condition):
-                return True
+        for i in range(fields_to_move, -1, -1):
+            row_result = np.roll(transformed_field[i], (fields_to_move-i))
+            transformed_field[i] = row_result
 
-    return False
-
-
-def transform_game_field(game_field):
-    fields_to_move = Y-1
-    columns_to_add = np.zeros([Y, fields_to_move])
-    transformed_field = np.hstack((game_field, np.atleast_2d(columns_to_add)))
-    print(transformed_field)
-
-    for i in range(fields_to_move, -1, -1):
-        row_result = np.roll(transformed_field[i], (fields_to_move-i))
-        transformed_field[i] = row_result
-
-    print(transformed_field)
-    return transformed_field
+        print(transformed_field)
+        return transformed_field
