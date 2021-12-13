24b4ef42023d8ed7b087379102ad0c8467552b2e
Michael Schweimer <schweimermichael@gmail.com>
2021-12-13 21:36:51 +0100

    Added vertical win condition + cleanup and code style

--- sources/connect_four.py
+++ sources/connect_four.py
@@ -1,19 +1,11 @@
 import numpy as np
 
 # init game field
-x = 7
-y = 6
+X = 7
+Y = 6
 win_condition = np.array([1, 1, 1, 1])
 
-field = np.zeros([y, x])
-
-# init test szenario
-# for i in range(2, 5):
-#    field[i][5] = 1
-
-field[1][2] = 1
-field[2][3] = 1
-field[3][4] = 1
+field = np.zeros([Y, X])
 
 
 def set_game_marker(xpos, ypos, player):
@@ -23,19 +15,21 @@ def set_game_marker(xpos, ypos, player):
 def scan_field():
     # checking game field x = 7, y = 6
     # print(field)
-    winner_found = check_wincondition(field, x, y)
+    winner_found = check_wincondition(field, X, Y)
 
     # check gamefield with transponed matrix
     # therefore rows will be columns
     # => logic for check could be reused
     if not winner_found:
-        winner_found = check_wincondition(field.T, y, x)
-        # print(field.T)
+        winner_found = check_wincondition(field.T, Y, X)
 
     if not winner_found:
-        # transformed_field = np.reshape(field, (8, 9))
-        # print(transformed_field)#
-        transform_game_field()
+        transformed_field = transform_game_field(field)
+        winner_found = check_wincondition(transformed_field, X, Y)
+
+    if not winner_found:
+        transformed_field = transform_game_field(field)
+        winner_found = check_wincondition(transformed_field.T, Y, X)
 
     return winner_found
 
@@ -49,10 +43,10 @@ def check_wincondition(game_field, x_max, y_max):
     return False
 
 
-def transform_game_field():
-    fields_to_move = y-1
-    columns_to_add = np.zeros([y, fields_to_move])
-    transformed_field = np.hstack((field, np.atleast_2d(columns_to_add)))
+def transform_game_field(game_field):
+    fields_to_move = Y-1
+    columns_to_add = np.zeros([Y, fields_to_move])
+    transformed_field = np.hstack((game_field, np.atleast_2d(columns_to_add)))
     print(transformed_field)
 
     for i in range(fields_to_move, -1, -1):
@@ -61,8 +55,3 @@ def transform_game_field():
 
     print(transformed_field)
     return transformed_field
-
-
-set_game_marker(1, 0, 1)
-result = scan_field()
-print(result)
